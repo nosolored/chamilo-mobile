76b3f40102ac0b6cf5f418300aa59da480e72a40
Juan Cortizas Ponte <48205899+juan-cortizas-ponte@users.noreply.github.com>
2022-06-18 01:34:50 +0200

    agregar foros

--- www/js/models/forum.js
+++ www/js/models/forum.js
@@ -4,19 +4,7 @@ define([
 ], function (DB, Backbone) {
     var ForumModel = Backbone.Model.extend({
         defaults: {
-			iid: 0,
-            c_id: 0,
-			s_id: 0,
-			forum_id: 0,
-            title: '',
-            description: '',
-			id_category: 0,
-			order: 0,
-			threads: 0,
-			posts: 0,
-			last_post_date: '',
-			last_poster: '',
-			image: ''
+            list_categories: []
         }
     });
     return ForumModel;
--- www/js/views/forum.js
+++ www/js/views/forum.js
@@ -20,59 +20,22 @@ define([
         var options = { dimBackground: true };
         SpinnerPlugin.activityStart(window.lang.LoadingScreen, options);
 
-        var url = campusModel.get('url') + '/plugin/chamilo_app/rest.php';
+        var url = campusModel.get('url') + '/main/webservices/api/v2.php';
         var getForums = $.post(url, {
-            action: 'getForumsList',
+            action: 'course_forum',
             username: campusModel.get('username'),
             api_key: campusModel.get('apiKey'),
-			user_id: campusModel.get('user_id'),
-			c_id: courseId,
-			s_id: sessionId
+            course: courseId,
+            session: sessionId
         });
 
         $.when(getForums).done(function (response) {
-            if (!response.status) {
+            if (response.error) {
                 SpinnerPlugin.activityStop();
                 return;
             }
-			var forums = response.forums.info_forum; 
-			cat_forum = response.forums.info_category;
 
-			for( i in forums ){
-				var forumData = forums[i];
-				//comprobamos si existe
-				//console.log(forumData);
-				var cid = forumData.iid;
-				if (forumsCollection.get(cid) == null) { 
-					forumsCollection.create({
-						iid: parseInt(forumData.iid),
-						c_id: parseInt(forumData.c_id),
-						s_id: parseInt(forumData.session_id),
-						forum_id: parseInt(forumData.forum_id),
-						title: forumData.forum_title,
-						description: forumData.forum_description,
-						id_category: parseInt(forumData.forum_category),
-						order: parseInt(forumData.forum_order),
-						threads: forumData.number_of_threads,
-						posts: forumData.number_of_posts,
-						last_post_date: forumData.last_post_date,
-						last_poster: forumData.last_poster,
-						image: forumData.forum_image
-					});
-				} else {
-					var forum = forumsCollection.get(cid);
-					forum.set({"title": forumData.forum_title});
-					forum.set({"description": forumData.forum_description});
-					forum.set({"id_categoy": forumData.forum_category});
-					forum.set({"order": forumData.forum_order});
-					forum.set({"threads": forumData.number_of_threads});
-					forum.set({"posts": forumData.number_of_posts});
-					forum.set({"last_post_date": forumData.last_post_date});
-					forum.set({"last_poster": forumData.last_poster});
-					forum.set({"image": forumData.forum_image});
-					forumsCollection.set(forum,{remove: false});
-				}
-            };
+            forumsCollection.set({"list_categories": response.data});
 
             SpinnerPlugin.activityStop();
 
@@ -131,7 +94,7 @@ define([
             forumsCollection.on('change', this.render, this);
         },
         render: function () {
-            this.el.innerHTML = this.template({collection: forumsCollection.toJSON(), c_id: courseId, s_id: sessionId, category: cat_forum});
+            this.el.innerHTML = this.template({collection: forumsCollection.toJSON(), c_id: courseId, s_id: sessionId});
 			return this;
         },
         events: {
