160ff683223b54925f87f762596b3438ca22f783
Juan Cortizas Ponte <48205899+juan-cortizas-ponte@users.noreply.github.com>
2022-06-18 22:37:43 +0200

    corregir espacio profile.js

--- www/js/views/profile.js
+++ www/js/views/profile.js
@@ -12,13 +12,13 @@ define([
     AlertView
 ) {
     var campusModel = null;
-	var profileModel = new ProfileModel();
+    var profileModel = new ProfileModel();
     
 
   var loadProfile = function () {
-	  console.log("funcion loadProfile");
+      console.log("funcion loadProfile");
         /*
-		if (!window.navigator.onLine) {
+        if (!window.navigator.onLine) {
             new AlertView({
                 model: {
                     message: window.lang.notOnLine
@@ -26,30 +26,30 @@ define([
             });
             return;
         }
-		*/
-		
-		console.log(profileModel);
-		
+        */
+
+        console.log(profileModel);
+
         var url = campusModel.get('url') + '/main/webservices/api/v2.php';
         var getCourses = $.post(url, {
             action: 'user_profile',
             username: campusModel.get('username'),
             api_key: campusModel.get('apiKey'),
-			user_id: campusModel.get('user_id')
+            user_id: campusModel.get('user_id')
         });
 
         $.when(getCourses).done(function (response) {
-			console.log(response);
+            console.log(response);
             if (response.error) {
                 return;
             }
 
             profileModel.set({"complete_name": response.data.fullName});
-			profileModel.set({"username": response.data.username});
-			profileModel.set({"email": response.data.email});
-			profileModel.set({"official_code": response.data.officialCode});
-			profileModel.set({"phone": response.data.phone});
-			profileModel.set({"picture_uri": response.data.pictureUri});
+            profileModel.set({"username": response.data.username});
+            profileModel.set({"email": response.data.email});
+            profileModel.set({"official_code": response.data.officialCode});
+            profileModel.set({"phone": response.data.phone});
+            profileModel.set({"picture_uri": response.data.pictureUri});
             profileModel.set({"extra": response.data.extra});
 
             if (response.data.length === 0) {
@@ -60,23 +60,22 @@ define([
                 });
                 return;
             }
-		});
+        });
     };
-	
 
     var ProfileView = Backbone.View.extend({
         el: 'body',
         template: _.template(ProfileTemplate),
         initialize: function () {
-			$(this.el).unbind();
+            $(this.el).unbind();
             campusModel = this.model;
-			console.log("initialize")
+            console.log("initialize")
             loadProfile();
-			profileModel.on('change', this.render, this);
+            profileModel.on('change', this.render, this);
         },
         render: function () {
-			console.log(profileModel.toJSON);
-			this.el.innerHTML = this.template(profileModel.toJSON());
+            console.log(profileModel.toJSON);
+            this.el.innerHTML = this.template(profileModel.toJSON());
 
             return this;
         }
